312010e8df64bb8f1a4a15b4502cced6f3d18ec0
chenleqi767 <chenleqi767@gmail.com>
2024-09-01 12:30:16 +0800

    Site updated: 2024-09-01 12:30:12

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/09/01/hello-world/","2024/09/01/信息公开/技术手册/","2024/09/01/信息公开/文档规范/"];function toRandomPost(){
+var posts=["2024/09/01/hello-world/","2024/09/01/信息公开/技术手册/","2024/09/01/信息公开/文档规范/","2024/09/01/信息公开/班级电子课表部署通知/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
